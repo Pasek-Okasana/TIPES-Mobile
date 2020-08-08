a72a1de3dbb24a4f07998eae73fa0683e18548b9
Pasek-Okasana <komangpasekokasana2gmail.com>
2020-08-08 18:50:52 +0800

    [Create] ==> Home 20 %

--- app/src/main/java/com/tipes/mobile/view/user/MainActivityUser.java
+++ app/src/main/java/com/tipes/mobile/view/user/MainActivityUser.java
@@ -1,16 +1,99 @@
 package com.tipes.mobile.view.user;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.tipes.mobile.R;
+import com.tipes.mobile.databinding.ActivityMainUserBinding;
+import com.tipes.mobile.model.MenuDashboardModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivityUser extends AppCompatActivity {
+    private String TAG = MainActivityUser.class.getSimpleName();
+
+    private ActivityMainUserBinding binding;
+    private List<MenuDashboardModel> mMenuList, mList;
+
+    private GridLayoutManager mLayoutManager;
+    private MainUserAdapter mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_user);
+        binding = ActivityMainUserBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        tambahDataMenu();
+        setMenu();
+        onClickItemMenu();
+    }
+
+    private void onClickItemMenu() {
+        mAdapter.setOnItemClickListener(new MainUserAdapter.ClickListener() {
+            @Override
+            public void onCardClick(View v, int position) {
+                makeLogI("Berhasil Diclik " + position);
+            }
+        });
+    }
+
+    private void setMenu() {
+        mList = new ArrayList<>();
+        mAdapter = new MainUserAdapter(mMenuList);
+        mLayoutManager = new GridLayoutManager(MainActivityUser.this, 2);
+        binding.recyclerHomeUser.setHasFixedSize(true);
+
+        binding.recyclerHomeUser.setLayoutManager(mLayoutManager);
+        binding.recyclerHomeUser.setAdapter(mAdapter);
+
+        mAdapter.notifyDataSetChanged();
+
+    }
+
+    private void tambahDataMenu() {
+        mMenuList = new ArrayList<>();
+        mMenuList.add(new MenuDashboardModel("Petunjuk", "", R.drawable.ic_folder_e_32dp));
+        mMenuList.add(new MenuDashboardModel("Kuisioner", "", R.drawable.ic_pencil_e_32dp));
+        mMenuList.add(new MenuDashboardModel("Nilai", "", R.drawable.ic_centang_e_32dp));
+        mMenuList.add(new MenuDashboardModel("Akun", "", R.drawable.ic_user_e_32dp));
+    }
+
+    /**
+     ==================== Make Toast
+     */
+    private void makeToast(String msg)
+    {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+    private void makeSnack(String msg){
+        Snackbar snackbar1 = Snackbar
+                .make(binding.getRoot(), msg, Snackbar.LENGTH_LONG)
+                .setDuration(6000);
+
+        snackbar1.show();
+        snackbar1.setAction("OK", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snackbar1.dismiss();
+            }
+        });
+    }
+    private void makeLogI(String msg)
+    {
+        Log.i(TAG, msg);
+    }
+    private void makeLogD(String msg)
+    {
+        Log.d(TAG, msg);
     }
 }
--- app/src/main/java/com/tipes/mobile/view/user/MainUserAdapter.java
+++ app/src/main/java/com/tipes/mobile/view/user/MainUserAdapter.java
@@ -1,4 +1,80 @@
 package com.tipes.mobile.view.user;
 
-public class MainUserAdapter  {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.tipes.mobile.databinding.ItemMenuDashboardBinding;
+import com.tipes.mobile.model.MenuDashboardModel;
+
+import java.util.List;
+
+public class MainUserAdapter extends RecyclerView.Adapter<MainUserAdapter.MainHolder> {
+    private List<MenuDashboardModel> mList;
+    private static ClickListener clickListener;
+
+    public MainUserAdapter(List<MenuDashboardModel> mList) {
+        this.mList = mList;
+    }
+
+    @NonNull
+    @Override
+    public MainHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        ItemMenuDashboardBinding binding = ItemMenuDashboardBinding.inflate(layoutInflater, parent, false);
+        return new MainHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MainHolder holder, int position) {
+        holder.bindToView(getItemPosition(position), position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public MenuDashboardModel getItemPosition(int position)
+    {
+        return  mList.get(position);
+    }
+
+    public class MainHolder extends RecyclerView.ViewHolder {
+        private ItemMenuDashboardBinding mBinding;
+        public MainHolder(ItemMenuDashboardBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+            mBinding.cardMenu.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION && clickListener != null) {
+                        clickListener.onCardClick(view, pos);
+                    }
+                }
+            });
+        }
+
+        public void bindToView(MenuDashboardModel itemPosition, int position) {
+            mBinding.imgLogo.setImageResource(itemPosition.getGambar());
+            mBinding.txtNama.setText(itemPosition.getNama());
+        }
+    }
+
+    /**
+     ===================================================================
+     Fungsi Item Bisa di klik
+     ===================================================================
+     **/
+    public void setOnItemClickListener(ClickListener clickListener) {
+        MainUserAdapter.clickListener = clickListener;
+    }
+
+    public interface ClickListener {
+        void onCardClick(View v, int position);
+    }
 }
